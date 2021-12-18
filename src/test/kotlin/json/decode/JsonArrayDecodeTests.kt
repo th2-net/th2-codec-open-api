5c1249ce7a0edd595bd788b8de325dc21858aaf2
Nikita Shaposhnikov <topru123@gmail.com>
2021-12-18 03:00:36 +0300

    Copyright update

--- src/test/kotlin/json/decode/JsonArrayDecodeTests.kt
+++ src/test/kotlin/json/decode/JsonArrayDecodeTests.kt
@@ -1,5 +1,5 @@
-package json.decode/*
- * Copyright 2020-2021 Exactpro (Exactpro Systems Limited)
+/*
+ * Copyright 2021-2022 Exactpro (Exactpro Systems Limited)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -13,6 +13,8 @@ package json.decode/*
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+package json.decode
 
 import com.exactpro.th2.codec.openapi.OpenApiCodec
 import com.exactpro.th2.codec.openapi.OpenApiCodec.Companion.HEADERS_FIELD
