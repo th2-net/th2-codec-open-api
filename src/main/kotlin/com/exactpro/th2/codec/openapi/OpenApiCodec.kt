caeae21e41e9a1344e085b7162899a882ea257c5
Nikita Shaposhnikov <topru123@gmail.com>
2021-12-19 16:29:20 +0300

    Decode request as well

--- src/main/kotlin/com/exactpro/th2/codec/openapi/OpenApiCodec.kt
+++ src/main/kotlin/com/exactpro/th2/codec/openapi/OpenApiCodec.kt
@@ -39,9 +39,11 @@ import com.google.protobuf.ByteString
 import io.swagger.v3.oas.models.OpenAPI
 import com.exactpro.th2.common.grpc.AnyMessage.KindCase.MESSAGE
 import com.exactpro.th2.common.grpc.AnyMessage.KindCase.RAW_MESSAGE
+import com.exactpro.th2.common.grpc.Message
 import com.exactpro.th2.common.message.getList
 import com.exactpro.th2.common.message.getString
 import com.exactpro.th2.common.message.messageType
+import io.swagger.v3.oas.models.media.Schema
 import mu.KotlinLogging
 import java.nio.charset.StandardCharsets.UTF_8
 
@@ -50,111 +52,10 @@ class OpenApiCodec(
     private val settings: OpenApiCodecSettings
 ) : IPipelineCodec {
 
-    private val messagesToSchema = mutableMapOf<String, HttpContainer>()
-
+    private val messagesToSchema = dictionary.toMap()
 
     init {
         SchemaWriter.createInstance(dictionary)
-        dictionary.paths.forEach { pathKey, pathsValue ->
-            pathsValue.getMethods().forEach { (methodKey, methodValue) ->
-                // Request
-                methodValue.requestBody?.content?.forEach { (typeKey, typeValue) ->
-                    val schema = dictionary.getEndPoint(typeValue.schema).apply {
-                        format = typeKey
-                    }
-
-                    messagesToSchema[combineName(listOf(pathKey, methodKey, typeKey))] =
-                        RequestContainer(pathKey, methodKey, methodValue.parameters, schema)
-                } ?: run {
-                    messagesToSchema[combineName(listOf(pathKey, methodKey))] =
-                        RequestContainer(pathKey, methodKey, methodValue.parameters, null)
-                }
-
-
-                // Response
-                methodValue.responses?.forEach { (responseKey, responseValue) ->
-                    responseValue.content?.forEach { (typeKey, typeValue) ->
-                        val schema = dictionary.getEndPoint(typeValue.schema).apply {
-                            format = typeKey
-                        }
-                        messagesToSchema[combineName(listOf(pathKey, methodKey, responseKey, typeKey))] =
-                            ResponseContainer(pathKey, methodKey, responseKey, schema)
-                    } ?: run {
-                        messagesToSchema[combineName(listOf(pathKey, methodKey, responseKey))] =
-                            ResponseContainer(pathKey, methodKey, responseKey, null)
-                    }
-                }
-            }
-        }
-        LOGGER.info { "Messages to encode: ${messagesToSchema.keys.joinToString(", ") { it }}" }
-    }
-
-    private fun combineName(steps: List<String>): String {
-        return buildString {
-            for (step in steps) {
-                step.split("{", "}", "-", "/", "_").forEach { word ->
-                    append(word.capitalize())
-                }
-            }
-        }
-    }
-
-    override fun decode(messageGroup: MessageGroup): MessageGroup {
-        val messages = messageGroup.messagesList
-
-        require(messages.size == 2) { "Message group must contain only 2 messages" }
-        require(messages[0].kindCase == MESSAGE) { "Message must be a raw message" }
-        require(messages[1].kindCase == RAW_MESSAGE) { "Message must be a raw message" }
-        val message = messages[0].message
-        val rawMessage = messages[1].rawMessage
-
-        val body = rawMessage.body.toByteArray().toString(UTF_8)
-
-        val builder = MessageGroup.newBuilder()
-        builder += message
-
-        when (message.messageType) {
-            "Response" -> {
-                val uri = requireNotNull(rawMessage.metadata.propertiesMap[URI_PROPERTY]) { "URI property in metadata of response is required" }
-                val method = requireNotNull(rawMessage.metadata.propertiesMap[METHOD_PROPERTY]?.lowercase()) { "Method property in metadata of response is required" }
-                val code = requireNotNull(message.getString(STATUS_CODE_FIELD)) { "Code status field required inside of http response message" }
-                val type = message.getList(HEADERS_FIELD)?.first { it.messageValue.getString("name") == "Content-Type" }?.messageValue?.getString("value")
-                    ?: "null"
-
-                val messageSchema = dictionary.paths[uri]?.getMethods()?.get(method)?.responses?.get(code)?.content?.get(type)?.schema?.run {
-                    dictionary.getEndPoint(this)
-                }
-
-                if (body.isNotEmpty()) {
-                    checkNotNull(messageSchema) { "Message with path $uri, method $method, code $code and type $type wasn't found" }
-
-                    val visitor = when (JsonSchemaTypes.getType(messageSchema.type)
-                        ?: error("Unsupported type of json schema ${messageSchema.type}")) {
-                        JsonSchemaTypes.ARRAY -> DecodeJsonArrayVisitor(body)
-                        JsonSchemaTypes.OBJECT -> DecodeJsonObjectVisitor(body)
-                    }
-                    SchemaWriter.instance.runCatching {
-                        traverse(visitor, messageSchema)
-                    }.onFailure {
-                        throw DecodeException(
-                            "Cannot parse json body of response with path $uri, method $method, code $code and type $type",
-                            it
-                        )
-                    }
-                    builder += visitor.getResult()
-                }
-
-            }
-            "Request" -> {
-                val uri = requireNotNull(rawMessage.metadata.propertiesMap[URI_PROPERTY]) { "URI property in metadata of response is required" }
-                val method = requireNotNull(rawMessage.metadata.propertiesMap[METHOD_PROPERTY]) { "Method property in metadata of response is required" }
-
-            }
-            else -> error("Unsupported message type: ${message.messageType}")
-        }
-
-
-        return builder.build()
     }
 
     override fun encode(messageGroup: MessageGroup): MessageGroup {
@@ -230,15 +131,145 @@ class OpenApiCodec(
         return builder.build()
     }
 
+    override fun decode(messageGroup: MessageGroup): MessageGroup {
+        val messages = messageGroup.messagesList
+
+        require(messages.size == 2) { "Message group must contain only 2 messages" }
+        require(messages[0].kindCase == MESSAGE) { "Message must be a raw message" }
+        require(messages[1].kindCase == RAW_MESSAGE) { "Message must be a raw message" }
+        val message = messages[0].message
+        val rawMessage = messages[1].rawMessage
+
+        val body = rawMessage.body.toByteArray().toString(UTF_8)
+
+        val builder = MessageGroup.newBuilder()
+        builder += message
+
+        when (message.messageType) {
+            RESPONSE_MESSAGE -> {
+                val uri = requireNotNull(rawMessage.metadata.propertiesMap[URI_PROPERTY]) { "URI property in metadata of response is required" }
+                val method = requireNotNull(rawMessage.metadata.propertiesMap[METHOD_PROPERTY]?.lowercase()) { "Method property in metadata of response is required" }
+                val code = requireNotNull(message.getString(STATUS_CODE_FIELD)) { "Code status field required inside of http response message" }
+                val type = message.getList(HEADERS_FIELD)?.first { it.messageValue.getString("name") == "Content-Type" }?.messageValue?.getString("value")
+                    ?: "null"
+
+                val messageSchema = dictionary.paths[uri]?.getMethods()?.get(method)?.responses?.get(code)?.content?.get(type)?.schema?.run {
+                    dictionary.getEndPoint(this)
+                }
+
+                if (body.isNotEmpty()) {
+                    checkNotNull(messageSchema) { "Response schema with path $uri, method $method, code $code and type $type wasn't found" }
+
+                    messageSchema.runCatching {
+                        builder += decodeSingle(body)
+                    }.onFailure {
+                        throw DecodeException(
+                            "Cannot parse json body of response with path $uri, method $method, code $code and type $type",
+                            it
+                        )
+                    }
+                }
+
+            }
+            REQUEST_MESSAGE -> {
+                val uri = requireNotNull(message.getString(URI_PROPERTY)) { "URI property in fields of request is required" }
+                val method = requireNotNull(message.getString(METHOD_PROPERTY)) { "Method property in fields of request is required" }
+                val type = message.getList(HEADERS_FIELD)?.first { it.messageValue.getString("name") == "Content-Type" }?.messageValue?.getString("value")
+                    ?: "null"
+
+                val messageSchema = dictionary.paths[uri]?.getMethods()?.get(method)?.requestBody?.content?.get(type)?.schema?.run {
+                    dictionary.getEndPoint(this)
+                }
+
+                if (body.isNotEmpty()) {
+                    checkNotNull(messageSchema) { "Request schema with path $uri, method $method and type $type wasn't found" }
+
+                    messageSchema.runCatching {
+                        builder += decodeSingle(body)
+                    }.onFailure {
+                        throw DecodeException(
+                            "Cannot parse json body of request with path $uri, method $method and type $type",
+                            it
+                        )
+                    }
+                }
+            }
+            else -> error("Unsupported message type: ${message.messageType}")
+        }
+
+
+        return builder.build()
+    }
+
+    private fun Schema<*>.decodeSingle(body: String): Message {
+        when (MessageFormat.getFormat(format) ?: error("Unsupported format of message $format")) {
+            MessageFormat.JSON -> {
+                val visitor = when (JsonSchemaTypes.getType(type)
+                    ?: error("Unsupported type of json schema ${type}")) {
+                    JsonSchemaTypes.ARRAY -> DecodeJsonArrayVisitor(body)
+                    JsonSchemaTypes.OBJECT -> DecodeJsonObjectVisitor(body)
+                }
+                SchemaWriter.instance.traverse(visitor, this)
+                return visitor.getResult()
+            }
+        }
+    }
+
+    private fun OpenAPI.toMap(): Map<String, HttpContainer> {
+        val map = mutableMapOf<String, HttpContainer>()
+        dictionary.paths.forEach { pathKey, pathsValue ->
+            pathsValue.getMethods().forEach { (methodKey, methodValue) ->
+                // Request
+                methodValue.requestBody?.content?.forEach { (typeKey, typeValue) ->
+                    val schema = dictionary.getEndPoint(typeValue.schema).apply {
+                        format = typeKey
+                    }
+
+                    map[combineName(listOf(pathKey, methodKey, typeKey))] =
+                        RequestContainer(pathKey, methodKey, methodValue.parameters, schema)
+                } ?: run {
+                    map[combineName(listOf(pathKey, methodKey))] =
+                        RequestContainer(pathKey, methodKey, methodValue.parameters, null)
+                }
+
+
+                // Response
+                methodValue.responses?.forEach { (responseKey, responseValue) ->
+                    responseValue.content?.forEach { (typeKey, typeValue) ->
+                        val schema = dictionary.getEndPoint(typeValue.schema).apply {
+                            format = typeKey
+                        }
+                        map[combineName(listOf(pathKey, methodKey, responseKey, typeKey))] =
+                            ResponseContainer(pathKey, methodKey, responseKey, schema)
+                    } ?: run {
+                        map[combineName(listOf(pathKey, methodKey, responseKey))] =
+                            ResponseContainer(pathKey, methodKey, responseKey, null)
+                    }
+                }
+            }
+        }
+        LOGGER.info { "Schemas to map: ${map.keys.joinToString(", ") { it }}" }
+        return map
+    }
+
+    private fun combineName(steps: List<String>): String {
+        return buildString {
+            for (step in steps) {
+                step.split("{", "}", "-", "/", "_").forEach { word ->
+                    append(word.capitalize())
+                }
+            }
+        }
+    }
+
     companion object {
         private val LOGGER = KotlinLogging.logger { }
         const val RESPONSE_MESSAGE = "Response"
+        const val REQUEST_MESSAGE = "Request"
         const val METHOD_PROPERTY = "method"
         const val URI_PROPERTY = "uri"
         const val CODE_PROPERTY = "code"
         const val STATUS_CODE_FIELD = "statusCode"
         const val HEADERS_FIELD = "headers"
-        const val REASON_FIELD = "reason"
-        private const val HTTP_PROTOCOL = "http"
     }
 }
